03eb65625d400fba7b96486a90e2cc407431d9f9
MarkusIngerslev <markusolsen2004@gmail.com>
2024-04-18 13:56:19 +0200

    Tilføjet de sider der kommer til at være på hjemmesiden

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-const Home = () => {
+function Home() {
     return (
         <div>
             <h1>Hello Markus</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, incidunt!</p>
         </div>
     );
-};
+}
 
 export default Home;
